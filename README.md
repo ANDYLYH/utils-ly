68afab8ea6c8a01ead449f5bfe080f96f85c4144
andylyh <360404369@qq.com>
2020-04-30 14:38:59 +0800

    -1

---
+++ README.md
@@ -0,0 +1,10 @@
+# timestampUtil
+格式化时间，获取2019-08-07 15:06:32或2019.08.07
+
+# install
+npm install -g timestampUtil
+
+
+#usage
+let mutil = require('./index')
+console.info(mutil.timestampToTime(1588216480709))
---
+++ index.js
@@ -0,0 +1,35 @@
+let utils = {
+  timestampToTime(time) {
+    if (time) {
+      var date = new Date(time);;
+      var Y = date.getFullYear() + "-";
+      var M = (date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1) + "-";
+      var D = date.getDate() < 10 ? "0" + date.getDate() + " " : date.getDate() + " ";
+      return Y + M + D;
+    }
+  },
+  timestampToTimepoint(time) {
+    if (time) {
+      var date = new Date(time);
+      var Y = date.getFullYear() + ".";
+      var M = (date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1) + ".";
+      var D = date.getDate() < 10 ? "0" + date.getDate() + " " : date.getDate() + " ";
+      return Y + M + D;
+    }
+  },
+  timestampToTimesecond(time) {
+    if (time) {
+      var date = new Date(time);
+      var Y = date.getFullYear() + "-";
+      var M = (date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1) + "-";
+      var D = date.getDate() < 10 ? "0" + date.getDate() + " " : date.getDate() + " ";
+      var h = date.getHours() + ":";
+      var mm = date.getMinutes();
+      if (mm < 10) {
+        mm = '0' + mm
+      }
+      return Y + M + D + h + mm;
+    }
+  },
+}
+module.exports = utils
